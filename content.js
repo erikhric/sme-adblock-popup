10dcd988874547eabfefc2521610c6ef8533415d
Erik Hric <erik.hric@gmail.com>
2021-08-30 13:35:54 +0200

    Fixed some edge cases, periodically ensure popup is hidden.

--- content.js
+++ content.js
@@ -1,1 +1,5 @@
-var elements = document.getElementsByTagName('body')[0].style = 'overflow: scroll;';
+setTimeout(() => {
+    document.getElementsByTagName('body')[0].style = 'overflow: scroll;';
+    document.getElementsByClassName('fc-ab-root')[0].style = 'visibility: hidden';
+    document.getElementsByClassName('fc-ab-root')[0].classList = [];
+}, 1000);
